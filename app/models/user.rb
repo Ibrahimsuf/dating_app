37d451cf814477e54aaca13ebff5240ae1b698f1
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2023-09-24 10:56:50 -0500

    Create profile associated with each user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one :profile
+  after_create :create_user_profile
+
+  private
+  def create_user_profile
+    Profile.create(user: self)
+  end
 end
