dbdeaae698dd822e5b3dd01f5e0d17397d8da11c
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2023-09-29 17:19:26 -0500

    Adding seed users and profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+# Creating users
+
+# db/seeds.rb
+
+# Create 20 users
+(1..20).each do |i|
+    User.create(
+      email: "user#{i}@example.com",
+      password: "password#{i}",
+      first_name: "First#{i}",
+      last_name: "Last#{i}"
+    )
+  end
+  
+  # Create profiles associated with users
+  User.all.each do |user|
+    user.profile.update(
+      bio: "Bio for #{user.first_name} #{user.last_name}"
+    )
+  end
+  
+
